26d7b371f1eeb533d56978b54a06ff6a7862ad31
alhabibdabbagh <58683636+alhabibdabbagh@users.noreply.github.com>
2020-04-20 15:10:01 +0300

    .

--- chapter-4-database-operations/src/main/java/db/connection/mongo/Main.java
+++ chapter-4-database-operations/src/main/java/db/connection/mongo/Main.java
@@ -6,15 +6,16 @@ import db.connection.mongo.connection.MongoDbConnection;
 public class Main {
 
 	public static String dbName = "employee";
-	
+
 	public static void main(String[] args) {
-		
+
 		// Get all database names
 		MongoDbConnection.getMongoDbConnection().getDatabaseNames().forEach(System.out::println);
-		
+
 		MongoDatabaseManager.createDB(dbName);
 
 		MongoDbConnection.getMongoDbConnection().close();
+
 	}
 
 }
